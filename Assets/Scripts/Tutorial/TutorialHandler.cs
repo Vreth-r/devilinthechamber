fe11f26cdf5818fc02fe8a76dc268930db3baa64
ymorts <minamicute@gmail.com>
2026-02-19 13:51:01 -0500

    added dialogue and a timer to start it

---
+++ Assets/Scripts/Tutorial/TutorialHandler.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using Yarn.Unity;
+
+public class TutorialHandler : MonoBehaviour
+{
+    float elapsedTime;
+    public PlayerControls controls;
+    public GameManager manager;
+    bool playerMoved = false;
+    public DialogueRunner dialogueRunner;
+    bool dialogueStarted = false;
+
+    public
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        controls = manager.controls;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveInput = Vector2.ClampMagnitude(controls.Player.Move.ReadValue<Vector2>(), 1f);
+        if (moveInput != Vector2.zero)
+        {
+            playerMoved = true; 
+        }
+
+        if (playerMoved)
+        {
+            elapsedTime += Time.deltaTime;
+        }
+
+        if (elapsedTime >= 4f && dialogueStarted == false)
+        {
+            dialogueRunner.StartDialogue("Start");
+            dialogueStarted = true;
+        }
+
+    }
+}
